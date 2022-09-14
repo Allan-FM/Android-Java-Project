1f65dc220cd2b0cf37a04d2711cdc637f680e2be
Allan <allanfelipedk@gmail.com>
2022-09-14 11:10:20 -0300

    incrementing a button and changing the text

--- app/src/main/java/com/allanfelipe/javaproject/MainActivity.java
+++ app/src/main/java/com/allanfelipe/javaproject/MainActivity.java
@@ -3,12 +3,19 @@ package com.allanfelipe.javaproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        msg = (TextView) findViewById(R.id.mensagem);
+    }
+
+    public void clicou(View view) {
+        msg.setText("Ola mundo!");
     }
 }
