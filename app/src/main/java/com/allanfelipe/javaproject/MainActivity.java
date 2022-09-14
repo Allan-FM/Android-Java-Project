ec0d4f6a7c12685083f5e5ea2db3658eaa24c413
Allan <allanfelipedk@gmail.com>
2022-09-13 22:20:45 -0300

    first commit

---
+++ app/src/main/java/com/allanfelipe/javaproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.allanfelipe.javaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
